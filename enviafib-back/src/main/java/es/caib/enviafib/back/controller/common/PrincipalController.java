b6239d3351b2177c45da92bd2ce2e1b0de5aeff2
fbosch-fundaciobit <fbosch@fundaciobit.org>
2022-05-06 14:32:41 +0200

    CORRECCIO: Establir les pagines inicials dels diferents rols a la url correcte #10

--- enviafib-back/src/main/java/es/caib/enviafib/back/controller/common/PrincipalController.java
+++ enviafib-back/src/main/java/es/caib/enviafib/back/controller/common/PrincipalController.java
@@ -77,11 +77,11 @@ public class PrincipalController {
 			 */
 
 			if ("admin".equals(pipella)) {
-				return new ModelAndView(new RedirectView("admin/usuari/list", true));
+				return new ModelAndView(new RedirectView("/admin/usuari/list/1", true));
 			}
 
 			if ("user".equals(pipella)) {
-				return new ModelAndView(new RedirectView("user/peticio/list", true));
+				return new ModelAndView(new RedirectView("/user/peticio/list/1", true));
 			}
 
 			if ("webdb".equals(pipella)) {
