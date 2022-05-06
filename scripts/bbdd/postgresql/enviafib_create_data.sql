2718a2308e0cc877f27b8558bc46fe689f370246
fbosch-fundaciobit <fbosch@fundaciobit.org>
2022-05-06 10:22:53 +0200

    Exportar i pujar al repositori la BBDD de EnviaFIB #9

---
+++ scripts/bbdd/postgresql/enviafib_create_data.sql
@@ -0,0 +1,8 @@
+
+
+
+INSERT INTO efi_idioma(idiomaid, nom, ordre) VALUES ('ca', 'Català', 0);
+INSERT INTO efi_idioma(idiomaid, nom, ordre) VALUES ('es', 'Castellano', 1);
+INSERT INTO efi_idioma(idiomaid, nom, ordre) VALUES ('en', 'English', 2);
+    
+    
