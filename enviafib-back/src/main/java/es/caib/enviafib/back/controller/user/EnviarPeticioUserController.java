b03f332f23e655cd59ca26fa115b15aa73ce9985
fbosch-fundaciobit <fbosch@fundaciobit.org>
2022-05-10 08:04:48 +0200

    Creació de funcions d'enviament de peticions de firma. #7

--- enviafib-back/src/main/java/es/caib/enviafib/back/controller/user/EnviarPeticioUserController.java
+++ enviafib-back/src/main/java/es/caib/enviafib/back/controller/user/EnviarPeticioUserController.java
@@ -4,18 +4,25 @@ import java.sql.Timestamp;
 
 import javax.ejb.EJB;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.fundaciobit.genapp.common.i18n.I18NException;
 import org.fundaciobit.genapp.common.query.Where;
-import org.springframework.core.annotation.AnnotationUtils;
+import org.fundaciobit.genapp.common.web.HtmlUtils;
+import org.fundaciobit.genapp.common.web.form.AdditionalButton;
+import org.fundaciobit.genapp.common.web.i18n.I18NUtils;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
 
 import es.caib.enviafib.back.controller.webdb.PeticioController;
 import es.caib.enviafib.back.form.webdb.PeticioFilterForm;
 import es.caib.enviafib.back.form.webdb.PeticioForm;
+import es.caib.enviafib.back.security.LoginException;
+import es.caib.enviafib.back.security.LoginInfo;
 import es.caib.enviafib.model.fields.PeticioFields;
 import es.caib.enviafib.model.fields.UsuariFields;
 import es.caib.enviafib.persistence.PeticioJPA;
@@ -32,6 +39,9 @@ public class EnviarPeticioUserController extends PeticioController {
 
 	@EJB(mappedName = es.caib.enviafib.ejb.UsuariService.JNDI_NAME)
 	protected es.caib.enviafib.ejb.UsuariService usuariEjb;
+	
+	@EJB(mappedName = es.caib.enviafib.logic.PeticioLogicaService.JNDI_NAME)
+	  protected es.caib.enviafib.logic.PeticioLogicaService peticioLogicaEjb;
 
 	@Override
 	public String getTileForm() {
@@ -76,8 +86,32 @@ public class EnviarPeticioUserController extends PeticioController {
 			peticioFilterForm.setVisibleExportList(false);
 			peticioFilterForm.addHiddenField(SOLICITANTID);
 			peticioFilterForm.addHiddenField(PETICIOID);
+			peticioFilterForm.addAdditionalButtonForEachItem(new AdditionalButton("fas fa-play", "posar_en_martxa", getContextWeb()+"/arrancar/{0}", "btn-success"));
 		}
 		return peticioFilterForm;
+	}
+	
+	@RequestMapping(value = "/arrancar/{peticioID}", method = RequestMethod.GET)
+	  public String arrancarPeticio(HttpServletRequest request,
+	    HttpServletResponse response, @PathVariable("peticioID") Long peticioID){
+		
+		try {
+			peticioLogicaEjb.arrancarPeticio(peticioID, LoginInfo.getInstance().getLanguage());
+			HtmlUtils.saveMessageSuccess(request, "Peticio amb Id: " + peticioID + " enviada correctament.");
+		} catch (LoginException e) {
+			// TODO Auto-generated catch block
+			String msg = "La sessio de l'usuari ha caducat."; 
+			HtmlUtils.saveMessageError(request, msg);
+			log.error(msg, e);
+		} catch (I18NException e) {
+			// TODO Auto-generated catch block
+			String msg = I18NUtils.getMessage(e);
+			HtmlUtils.saveMessageError(request, msg);
+			log.error(msg, e);
+		}
+		
+		return "redirect:" + getContextWeb() + "/list";
+		
 	}
 
 }
