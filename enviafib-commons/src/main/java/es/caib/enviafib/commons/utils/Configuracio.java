7be3e65bb438d326d4b76598fb71ad653ae96955
fbosch-fundaciobit <fbosch@fundaciobit.org>
2022-05-19 13:49:01 +0200

    Update per seguir els casnvis de Genapp

--- enviafib-commons/src/main/java/es/caib/enviafib/commons/utils/Configuracio.java
+++ enviafib-commons/src/main/java/es/caib/enviafib/commons/utils/Configuracio.java
@@ -1,6 +1,5 @@
 package es.caib.enviafib.commons.utils;
 
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -17,154 +16,177 @@ import java.util.Properties;
  */
 public class Configuracio implements Constants {
 
-    private static final Logger LOG = LoggerFactory.getLogger(Configuracio.class);
+	private static final Logger LOG = LoggerFactory.getLogger(Configuracio.class);
 
-    private static final Properties fileProperties = new Properties();
-    
-    private static final Properties fileAndSystemProperties = new Properties();
+	private static final Properties fileProperties = new Properties();
 
-    /*
-     * Agafa els fitxers de propietats definits a l'standalone
-     *
-     * Seguim els estandars de la CAIB 
-     */
-    public static Properties getFilesProperties() {
-        
-        if (fileProperties.isEmpty()) {
+	private static final Properties fileAndSystemProperties = new Properties();
+
+	/*
+	 * Agafa els fitxers de propietats definits a l'standalone
+	 *
+	 * Seguim els estandars de la CAIB
+	 */
+	public static Properties getFilesProperties() {
+
+		if (fileProperties.isEmpty()) {
 			// matches the property name as defined in the system-properties element in
 			// WildFly
-			try {
-				String property = Constants.ENVIAFIB_PROPERTY_BASE + "properties";
-				loadPropertyFile(property);
-				
+			String property = Constants.ENVIAFIB_PROPERTY_BASE + "properties";
+			loadPropertyFile(property);
+
 				String propertySystem = Constants.ENVIAFIB_PROPERTY_BASE + "system.properties";
-				loadPropertyFile(propertySystem);
-				
-			} catch (FileNotFoundException e) {
-				LOG.error("El fitxer de propietats no esta definit", e);
+			loadPropertyFile(propertySystem);
+		}
 
-			} catch (NullPointerException e) {
-				LOG.error("Propietat sense valor", e);
-				
-			} catch (IOException e) {
-				LOG.error("No es pot carregar algun dels fitxers de propietats ... ", e);
-			}
-        }
-        
-        return fileProperties;
+		return fileProperties;
 
-    }
+	}
 
-	public static void loadPropertyFile(String property) throws FileNotFoundException, IOException {
+	public static void loadPropertyFile(String property) {
+
 		String propertyFile = System.getProperty(property);
 
-		if (propertyFile.equals("")) {
-			throw new NullPointerException("No esta definida la propietat: " + property);
+		if (propertyFile == null) {
+			throw new RuntimeException("No existeix la propietat: " + property
+					+ " al fitxer standalone.xml. S'hauria d'incloure aquesta propietat a l'etiqueta <system-properties> del fitxer standalone");
+		}
+
+		if (propertyFile.trim().length() == 0) {
+			throw new RuntimeException("La propietat: " + property
+					+ " del fitxer standalone.xml no te valor. Se li ha de posar el fitxer corresponent a la propietat al fitxer standalone");
 		}
 
 		File File = new File(propertyFile);
-		if (!File.exists()) {
-			throw new FileNotFoundException(File.getAbsolutePath());
+//		if (!File.exists()) {
+//			throw new RuntimeException("La propietat "File.getAbsolutePath());
+//		}
+
+		try {
+			fileProperties.load(new FileInputStream(File));
+
+		} catch (FileNotFoundException e) {
+			throw new RuntimeException("La propietat: " + property
+					+ " del fitxer standalone apunta a un fitxer que no existeix (" + propertyFile + ")");
+
+		} catch (IOException e) {
+			throw new RuntimeException("La propietat: " + property + " del fitxer standalone apunta a un fitxer("
+					+ propertyFile + ") que no es pot llegir:" + e.getMessage(), e);
 		}
-		fileProperties.load(new FileInputStream(File));
 	}
 
-    
-    public static Properties getSystemAndFileProperties() {
 
-        if (fileAndSystemProperties.isEmpty()) {
-            fileAndSystemProperties.putAll(getFilesProperties());
-            fileAndSystemProperties.putAll(System.getProperties());
-        }
-        return fileAndSystemProperties;
-    }
 
-    public static String getProperty(String key) {
+	public static Properties getSystemAndFileProperties() {
 
-        return  getFilesProperties().getProperty(key);
+		if (fileAndSystemProperties.isEmpty()) {
+			fileAndSystemProperties.putAll(getFilesProperties());
+			fileAndSystemProperties.putAll(System.getProperties());
+		}
+		return fileAndSystemProperties;
+	}
 
-    }
+	public static String getProperty(String key) {
 
-    public static String getProperty(String key, String def) {
-        
-        return getFilesProperties().getProperty(key, def);
+		return getFilesProperties().getProperty(key);
 
-    }
+	}
 
-    public static boolean isDesenvolupament() {
+	public static String getProperty(String key, String def) {
+
+		return getFilesProperties().getProperty(key, def);
+
+	}
+
+	public static boolean isDesenvolupament() {
 
         return Boolean.parseBoolean(getProperty(ENVIAFIB_PROPERTY_BASE + "development"));
     }
 
-    public static boolean isCAIB() {
+	public static boolean isCAIB() {
         return Boolean.parseBoolean(getProperty(ENVIAFIB_PROPERTY_BASE + "iscaib"));
     }
 
-    public static String getAppUrl() {
+	public static String getAppUrl() {
         return getProperty(ENVIAFIB_PROPERTY_BASE + "url");
     }
 
-    public static String getAppEmail() {
+	public static String getAppEmail() {
         return getProperty(ENVIAFIB_PROPERTY_BASE + "email.from");
     }
 
-    public static String getAppName() {
+	public static String getAppName() {
         return getProperty(ENVIAFIB_PROPERTY_BASE + "name", "EnviaFIB");
     }
 
-    public static String getDefaultLanguage() {
+	public static String getDefaultLanguage() {
         return getProperty(ENVIAFIB_PROPERTY_BASE + "defaultlanguage", "ca");
     }
 
-    public static byte[] getEncryptKey() {
+	public static byte[] getEncryptKey() {
         return getProperty(ENVIAFIB_PROPERTY_BASE + "encryptkey", "0123456789123456").getBytes();
     }
 
-    public static Long getMaxUploadSizeInBytes() {
+	public static Long getMaxUploadSizeInBytes() {
         return Long.getLong(ENVIAFIB_PROPERTY_BASE + "maxuploadsizeinbytes");
     }
 
-    public static Long getMaxFitxerAdaptatSizeInBytes() {
+	public static Long getMaxFitxerAdaptatSizeInBytes() {
         return Long.getLong(ENVIAFIB_PROPERTY_BASE + "maxfitxeradaptatsizeinbytes");
     }
 
-    public static File getFilesDirectory() {
-    	
+	public static File getFilesDirectory() {
         String path = getProperty(ENVIAFIB_PROPERTY_BASE + "filesdirectory");
-        if(path == null) {
-        	throw new RuntimeException("No existeix la propietat 'filesdirectory' al fitxer 'system.properties'. \n"
-        			+ "S'hauria d'anar al fitxer '.system.properties' de JBoss standalone/deployments i incloure la propietat 'filesdirectory' amb una ruta al directori on l'aplició gestionara els fitxers.");
-        }else if(path.isEmpty()){
-        	throw new RuntimeException("No s'ha definit la propietat 'filesdirectory' al fitxer 'system.properties'. \n"
-        			+ "S'hauria d'anar al fitxer '.system.properties' de JBoss standalone/deployments i donar valor a la propietat 'filesdirectory' amb una ruta al directori on l'aplició gestionara els fitxers.");
+        if (path == null) {
+            throw new RuntimeException("No existeix la propietat " + "'filesdirectory'" + " al fitxer "
+                    + System.getProperty(ENVIAFIB_PROPERTY_BASE + "system.properties")
+                    + ". S'hauria d'anar al fitxer "
+                    + System.getProperty(ENVIAFIB_PROPERTY_BASE + "system.properties")
+                    + " de JBoss standalone/deployments i incloure la propietat " + "'filesdirectory'"
+                    + " amb una ruta al directori on l'aplició gestionara els fitxers.");
+        }
+
+        if (path.isEmpty()) {
+            throw new RuntimeException("No s'ha definit la propietat " + "'filesdirectory'" + " al fitxer "
+                    + System.getProperty(ENVIAFIB_PROPERTY_BASE + "system.properties")
+                    + ". S'hauria d'anar al fitxer "
+                    + System.getProperty(ENVIAFIB_PROPERTY_BASE + "system.properties")
+                    + " de JBoss standalone/deployments i donar valor a la propietat " + "'filesdirectory'"
+                    + " amb una ruta al directori on l'aplició gestionara els fitxers.");
         }
         
         File filesFolder = new File(path);
         
-        if(!filesFolder.exists()) {
-        	throw new RuntimeException("El directori indicat a la propietat 'filesdirectory' del fitxer 'system.properties' no existeix. \n "
-        			+ "S'hauria de modificar la ruta indicada al fitxer '.system.properties' de JBoss standalone/deployments per la d'un directori existent, o crear un directori amb la ruta: " + path);
-        }else if(!filesFolder.isDirectory()) {
-        	throw new RuntimeException("El directori indicat a la propietat 'filesdirectory' del fitxer 'system.properties' no es un directori, probablement es tracti d'un fitxer. \n"
-        			+ "S'hauria de modificar la ruta indicada al fitxer '.system.properties' de JBoss standalone/deployments per la d'un directori existent.");
+        if (!filesFolder.exists()) {
+            throw new RuntimeException("El directori indicat a la propietat " + "'filesdirectory'" + " del fitxer "
+                    + System.getProperty(ENVIAFIB_PROPERTY_BASE + "system.properties")
+                    + " no existeix. S'hauria de modificar la ruta indicada per la d'un directori existent, o crear un directori amb la ruta: "
+                    + path);
         }
-        
+
+        if (!filesFolder.isDirectory()) {
+            throw new RuntimeException("El directori indicat a la propietat " + "'filesdirectory'" + " del fitxer "
+                    + System.getProperty(ENVIAFIB_PROPERTY_BASE + "system.properties")
+                    + " no es un directori, probablement es tracti d'un fitxer. S'hauria de modificar la ruta indicada per la d'un directori existent.");
+        }
         return new File(path);
-    	
+
     }
 
-    public static String getFileSystemManager() {
+	public static String getFileSystemManager() {
         return getProperty(ENVIAFIB_PROPERTY_BASE + "filesystemmanagerclass");
     }
-    
-    public static String getPortafibGatewayV2() {
+	public static String getPortafibGatewayV2() {
         return getProperty(ENVIAFIB_PROPERTY_BASE + "portafibgatewayv2");
     }
+    
     public static String getPortafibUsername() {
         return getProperty(ENVIAFIB_PROPERTY_BASE + "portafibusername");
     }
+    
     public static String getPortafibPassword() {
         return getProperty(ENVIAFIB_PROPERTY_BASE + "portafibpassword");
     }
 
+	
 }
