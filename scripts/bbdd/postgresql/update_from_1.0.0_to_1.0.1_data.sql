e4c4a10529320b479251266886102991e2ebca4c
Antoni Nadal <anadal@fundaciobit.org>
2022-06-22 11:46:53 +0200

    Canviar estat REBUTJAT per ERROR #82

--- scripts/bbdd/postgresql/update_from_1.0.0_to_1.0.1_data.sql
+++ scripts/bbdd/postgresql/update_from_1.0.0_to_1.0.1_data.sql
@@ -4,4 +4,4 @@
 ---  Canviar estat REBUTJAT per ERROR #82 
 ---
 
-UPDATE efi_peticio SET errormsg='Petició Rebutjada'  WHERE estat=4 AND errormsg is not null;
+UPDATE efi_peticio SET errormsg='Petició Rebutjada'  WHERE estat=4 AND errormsg is null;
