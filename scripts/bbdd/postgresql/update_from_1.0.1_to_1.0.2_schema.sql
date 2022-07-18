c014a4d83ca892f1cc673bcf1deb10f63f655f99
pautrias <ptrias@fundaciobit.org>
2022-07-18 09:45:24 +0200

    Update fitxer d'updates

--- scripts/bbdd/postgresql/update_from_1.0.1_to_1.0.2_schema.sql
+++ scripts/bbdd/postgresql/update_from_1.0.1_to_1.0.2_schema.sql
@@ -15,3 +15,18 @@ ALTER TABLE efi_usuari
   ADD COLUMN idiomaid character varying(5) NOT NULL DEFAULT 'ca';
 ALTER TABLE efi_usuari
   ADD CONSTRAINT efi_usuari_idioma_fk FOREIGN KEY (idiomaid) REFERENCES efi_idioma (idiomaid) ON UPDATE NO ACTION ON DELETE NO ACTION;
+
+
+
+---
+--- 14/07/2022 - Posar el camp destinatariNif nullable #102
+---
+ALTER TABLE efi_peticio
+   ALTER COLUMN destinatarinif DROP NOT NULL;
+
+--
+-- 14/07/2022 - Afegir a base de dades camp "reason" per autofirma #101
+--
+ALTER TABLE efi_peticio
+   ADD COLUMN reason character varying(255);
+
