1ea1d6a1837b1e71ef9e1929f1beb3961217d274
Antoni Nadal <anadal@fundaciobit.org>
2022-08-09 10:49:47 +0200

    Errors de BBDD al Actualitzar el Projecte amb GenAPP #144

--- enviafib-persistence/src/main/java/es/caib/enviafib/persistence/InfoArxiuJPA.java
+++ enviafib-persistence/src/main/java/es/caib/enviafib/persistence/InfoArxiuJPA.java
@@ -6,6 +6,7 @@ import javax.persistence.Column;
 import java.util.HashSet;
 import javax.persistence.Entity;
 import javax.persistence.GenerationType;
+import javax.persistence.Index;
 import javax.persistence.GeneratedValue;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.FetchType;
@@ -15,7 +16,8 @@ import javax.persistence.Id;
 
 
 @Entity(name = "InfoArxiuJPA")
-@Table(name = "efi_infoarxiu" )
+@Table(name = "efi_infoarxiu" , indexes = { 
+        @Index(name="efi_infoarxiu_pk_i", columnList = "infoarxiuid")})
 @SequenceGenerator(name="INFOARXIU_SEQ", sequenceName="efi_infoarxiu_seq", allocationSize=1, initialValue=1000)
 @javax.xml.bind.annotation.XmlRootElement
 public class InfoArxiuJPA implements InfoArxiu {
