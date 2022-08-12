0468783547d5bc643af5a3fec05aa1a3d299cb09
Antoni Nadal <anadal@fundaciobit.org>
2022-08-12 12:58:55 +0200

    Revisió de Seqüències https://github.com/GovernIB/genapp/issues/118

--- scripts/bbdd/postgresql/update_from_1.0.2_to_1.0.3_schema.sql
+++ scripts/bbdd/postgresql/update_from_1.0.2_to_1.0.3_schema.sql
@@ -4,3 +4,13 @@
 
 ALTER TABLE efi_grup
    ALTER COLUMN nom SET NOT NULL;
+   
+   
+---
+--- 12/08/2022 Revisió de Seqüències - https://github.com/GovernIB/genapp/issues/118
+---
+   
+SELECT setval('efi_peticio_seq', 1000, true);
+SELECT setval('efi_plugin_seq', 1000, true);
+SELECT setval('efi_seriedocumental_seq', 1000, true);
+SELECT setval('efi_usuari_seq', 1000, true);
