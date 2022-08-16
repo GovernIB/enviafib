ad9bf112f1feae18cc0772f03ac2721b6819f0ca
fbosch-fundaciobit <fbosch@fundaciobit.org>
2022-08-16 08:49:31 +0200

    Afegir camps Procediment Nom i Procediment codi a taula efi_seriedocumental #157 (Scripts Oracle)

--- scripts/bbdd/oracle/update_from_1.0.2_to_1.0.3_schema.sql
+++ scripts/bbdd/oracle/update_from_1.0.2_to_1.0.3_schema.sql
@@ -4,3 +4,28 @@
 
 ALTER TABLE efi_grup
    MODIFY (nom NOT NULL);
+
+
+---
+--- 11/08/2022 - Afegir camps Procediment Nom i Procediment codi a taula efi_seriedocumental #157
+---
+
+
+ALTER TABLE efi_seriedocumental
+  ADD procedimentnom VARCHAR(255);
+ALTER TABLE efi_seriedocumental
+  ADD procedimentcodi VARCHAR(255);
+
+
+UPDATE efi_seriedocumental
+SET procedimentcodi = 'organo1_PRO_123456789'
+WHERE procedimentcodi IS NULL;
+
+UPDATE efi_seriedocumental
+SET procedimentnom='Subvenciones empleo'
+WHERE procedimentnom IS NULL;
+
+ALTER TABLE efi_seriedocumental
+   MODIFY (procedimentnom NOT NULL);
+ALTER TABLE efi_seriedocumental
+   MODIFY (procedimentcodi NOT NULL);
