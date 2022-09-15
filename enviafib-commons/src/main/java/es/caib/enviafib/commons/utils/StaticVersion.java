f19213698c686ff487a6c1d26e80e75ef9ed1140
fbosch-fundaciobit <fbosch@fundaciobit.org>
2022-09-15 08:49:51 +0200

    Obrir versió #2

--- enviafib-commons/src/main/java/es/caib/enviafib/commons/utils/StaticVersion.java
+++ enviafib-commons/src/main/java/es/caib/enviafib/commons/utils/StaticVersion.java
@@ -4,7 +4,7 @@ package es.caib.enviafib.commons.utils;
 */
 public interface StaticVersion {
 
-    public static final String VERSION="1.0.3";
+    public static final String VERSION="1.0.4";
 
     public static final String PROJECT_NAME="enviafib";
 
