ee207dc14ae7ecd877700deece08d84a8578e298
pautrias <ptrias@fundaciobit.org>
2022-12-01 10:36:52 +0100

    Quaderns de Càrrega per EnviaFIB 1.0.5 #270

---
+++ doc/QuadernDeCarrega/enviafib_1.0.5_20221128/fitxers/02_tables.sql
@@ -0,0 +1,161 @@
+
+    create table efi_fitxer (
+       fitxerid number(19,0) not null,
+        descripcio varchar2(1000 char),
+        mime varchar2(255 char) not null,
+        nom varchar2(255 char) not null,
+        tamany number(19,0) not null
+    );
+
+    create table efi_grup (
+       grupid number(19,0) not null,
+        descripcio varchar2(255 char),
+        nom varchar2(255 char) not null
+    );
+
+    create table efi_grupusuari (
+       grupusuariid number(19,0) not null,
+        grupid number(19,0) not null,
+        usuariid number(19,0) not null
+    );
+
+    create table efi_idioma (
+       idiomaid varchar2(5 char) not null,
+        nom varchar2(50 char) not null,
+        ordre number(10,0) default 0 not null,
+        suportat number(1,0) not null
+    );
+
+    create table efi_infoarxiu (
+       infoarxiuid number(19,0) not null,
+        arxiudocumentid varchar2(255 char),
+        arxiuexpedientid varchar2(255 char),
+        csv varchar2(255 char),
+        csvgenerationdefinition varchar2(255 char),
+        csvvalidationweb varchar2(255 char),
+        enifileurl varchar2(255 char),
+        originalfileurl varchar2(255 char),
+        printableurl varchar2(255 char),
+        validationfileurl varchar2(255 char)
+    );
+
+    create table efi_infosignatura (
+       infosignaturaid number(19,0) not null,
+        checkadministrationidofsigner number(1,0),
+        checkdocumentmodifications number(1,0),
+        checkvalidationsignature number(1,0),
+        eniperfilfirma varchar2(255 char),
+        enirolfirma varchar2(255 char),
+        enisignlevel varchar2(255 char),
+        enisigneradministrationid varchar2(255 char),
+        enisignername varchar2(255 char),
+        enitipofirma varchar2(255 char),
+        policyincluded number(1,0),
+        signalgorithm varchar2(255 char),
+        signmode number(10,0),
+        signoperation number(10,0) not null,
+        signtype varchar2(255 char) not null,
+        signaturestablelocation number(10,0),
+        timestampincluded number(1,0)
+    );
+
+    create table efi_menu (
+       menuid number(19,0) not null,
+        actiu number(1,0) not null,
+        ajudamenuid number(19,0) not null,
+        descripcio varchar2(255 char),
+        grupid number(19,0),
+        nom varchar2(255 char) not null,
+        ordre number(10,0) not null,
+        parametrecombo varchar2(255 char),
+        parametretext clob,
+        tipus number(10,0) not null,
+        titolmenuid number(19,0) not null
+    );
+
+    create table efi_organitzacio (
+       organitzacioid number(19,0) not null,
+        codiconselleria varchar2(100 char),
+        codidirecciogeneral varchar2(100 char),
+        tipus varchar2(100 char),
+        valor varchar2(255 char)
+    );
+
+    create table efi_peticio (
+       peticioid number(19,0) not null,
+        arxiufuncionariusername varchar2(255 char),
+        arxiuoptparamexpedientid varchar2(255 char),
+        arxiuoptparamprocedimentcodi varchar2(255 char),
+        arxiuoptparamprocedimentnom varchar2(255 char),
+        arxiuoptparamseriedocumental varchar2(255 char),
+        arxiuparamfuncionaridir3 varchar2(255 char),
+        arxiuparamfuncionarinif varchar2(255 char),
+        arxiuparamfuncionarinom varchar2(255 char),
+        arxiureqparamciutadanif varchar2(15 char),
+        arxiureqparamciutadanom varchar2(255 char),
+        arxiureqparamdocestatelabora varchar2(4 char),
+        arxiureqparaminteressats varchar2(255 char),
+        arxiureqparamorgans varchar2(255 char),
+        arxiureqparamorigen number(10,0),
+        datacreacio timestamp not null,
+        datafinal timestamp,
+        destinatarinif varchar2(50 char),
+        errorexception clob,
+        errormsg varchar2(255 char),
+        estat number(10,0) default 1 not null,
+        fitxer_firmatid number(19,0),
+        fitxerid number(19,0) not null,
+        idiomadoc varchar2(30 char) not null,
+        idiomaid varchar2(5 char) not null,
+        infoarxiuid number(19,0),
+        infosignaturaid number(19,0),
+        nom varchar2(255 char),
+        peticioportafirmes varchar2(255 char),
+        reason varchar2(255 char),
+        solicitantid number(19,0) not null,
+        tipus number(10,0) default 0 not null,
+        tipusdocumental varchar2(100 char) not null
+    );
+
+    create table efi_plugin (
+       pluginid number(19,0) not null,
+        actiu number(1,0) not null,
+        classe varchar2(255 char),
+        descripcio varchar2(255 char) not null,
+        nom varchar2(255 char) not null,
+        properties clob,
+        tipus number(10,0) not null
+    );
+
+    create table efi_seriedocumental (
+       seriedocumentalid number(19,0) not null,
+        nom varchar2(256 char) not null,
+        procedimentcodi clob not null,
+        procedimentnom clob not null,
+        tipusdocumental varchar2(256 char)
+    );
+
+    create table efi_traduccio (
+       traduccioid number(19,0) not null
+    );
+
+    create table efi_traducciomap (
+       traducciomapid number(19,0) not null,
+        valor varchar2(4000 char),
+        idiomaid varchar2(255 char) not null
+    );
+
+    create table efi_usuari (
+       usuariid number(19,0) not null,
+        email varchar2(256 char) not null,
+        idiomaid varchar2(5 char) default 'ca' not null,
+        llinatge1 varchar2(256 char) not null,
+        llinatge2 varchar2(256 char),
+        nif varchar2(50 char) not null,
+        nom varchar2(256 char) not null,
+        username varchar2(100 char) not null
+    );
+
+
+
+
---
+++ doc/QuadernDeCarrega/enviafib_1.0.5_20221128/fitxers/03_constraints.sql
@@ -0,0 +1,114 @@
+
+ -- INICI PKs
+    alter table efi_fitxer add constraint efi_fitxer_pk primary key (fitxerid);
+
+    alter table efi_grup add constraint efi_grup_pk primary key (grupid);
+
+    alter table efi_grupusuari add constraint efi_grupusuari_pk primary key (grupusuariid);
+
+    alter table efi_idioma add constraint efi_idioma_pk primary key (idiomaid);
+
+    alter table efi_infoarxiu add constraint efi_infoarxiu_pk primary key (infoarxiuid);
+
+    alter table efi_infosignatura add constraint efi_infosignatura_pk primary key (infosignaturaid);
+
+    alter table efi_menu add constraint efi_menu_pk primary key (menuid);
+
+    alter table efi_organitzacio add constraint efi_organitzacio_pk primary key (organitzacioid);
+
+    alter table efi_peticio add constraint efi_peticio_pk primary key (peticioid);
+
+    alter table efi_plugin add constraint efi_plugin_pk primary key (pluginid);
+
+    alter table efi_seriedocumental add constraint efi_seriedocumental_pk primary key (seriedocumentalid);
+
+    alter table efi_traduccio add constraint efi_traduccio_pk primary key (traduccioid);
+
+    alter table efi_traducciomap add constraint efi_traducmap_pk primary key (traducciomapid, idiomaid);
+
+    alter table efi_usuari add constraint efi_usuari_pk primary key (usuariid);
+
+ -- FINAL PKs
+
+
+ -- INICI FKs
+
+    alter table efi_grupusuari 
+       add constraint efi_grupusuari_grup_grupid_fk 
+       foreign key (grupid) 
+       references efi_grup;
+
+    alter table efi_grupusuari 
+       add constraint efi_grupusuari_usuari_usuar_fk 
+       foreign key (usuariid) 
+       references efi_usuari;
+
+    alter table efi_menu 
+       add constraint efi_menu_traduccio_ajuda_fk 
+       foreign key (ajudamenuid) 
+       references efi_traduccio;
+
+    alter table efi_menu 
+       add constraint efi_menu_grup_grupid_fk 
+       foreign key (grupid) 
+       references efi_grup;
+
+    alter table efi_menu 
+       add constraint efi_menu_traduccio_titol_fk 
+       foreign key (titolmenuid) 
+       references efi_traduccio;
+
+    alter table efi_peticio 
+       add constraint efi_peticio_fitxer_fitxer_fk 
+       foreign key (fitxerid) 
+       references efi_fitxer;
+
+    alter table efi_peticio 
+       add constraint efi_peticio_fitxer_ffirm_fk 
+       foreign key (fitxer_firmatid) 
+       references efi_fitxer;
+
+    alter table efi_peticio 
+       add constraint efi_peticio_idioma_idiid_fk 
+       foreign key (idiomaid) 
+       references efi_idioma;
+
+    alter table efi_peticio 
+       add constraint efi_peticio_infoarxiu_infoa_fk 
+       foreign key (infoarxiuid) 
+       references efi_infoarxiu;
+
+    alter table efi_peticio 
+       add constraint efi_peticio_infosign_fk 
+       foreign key (infosignaturaid) 
+       references efi_infosignatura;
+
+    alter table efi_peticio 
+       add constraint efi_peticio_usuari_soli_fk 
+       foreign key (solicitantid) 
+       references efi_usuari;
+
+    alter table efi_traducciomap 
+       add constraint efi_traducmap_traduccio_fk 
+       foreign key (traducciomapid) 
+       references efi_traduccio;
+
+    alter table efi_usuari 
+       add constraint efi_usuari_idioma_fk 
+       foreign key (idiomaid) 
+       references efi_idioma;
+ -- FINAL FKs
+
+
+ -- INICI UNIQUEs
+
+    alter table efi_grupusuari 
+       add constraint efi_grupusuari_usuari_grup_uk unique (usuariid, grupid);
+
+    alter table efi_seriedocumental 
+       add constraint UK_ox1aosn2t9fscv2lv01lr10tg unique (tipusdocumental);
+
+    alter table efi_usuari 
+       add constraint UK_rhi053fw7q637iv8ohhiasjad unique (nif);
+ -- FINAL UNIQUEs
+
