c3230467fe98c4ed354df1887e83eb67aefdfb6b
pautrias <ptrias@fundaciobit.org>
2023-02-14 14:02:03 +0100

    Obrir versió #2

--- enviafib-commons/src/main/java/es/caib/enviafib/commons/utils/StaticVersion.java
+++ enviafib-commons/src/main/java/es/caib/enviafib/commons/utils/StaticVersion.java
@@ -4,7 +4,7 @@ package es.caib.enviafib.commons.utils;
 */
 public interface StaticVersion {
 
-    public static final String VERSION="1.0.5";
+    public static final String VERSION="1.0.6";
 
     public static final String PROJECT_NAME="enviafib";
 
