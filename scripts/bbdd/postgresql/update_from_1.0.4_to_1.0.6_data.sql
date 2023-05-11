a8c3cb762de400e8816632bbd2053414b7cf466d
pautrias <ptrias@fundaciobit.org>
2023-05-11 11:32:15 +0200

    Revisar com s'informen les metadades mínimes obligatòries #266

--- scripts/bbdd/postgresql/update_from_1.0.4_to_1.0.6_data.sql
+++ scripts/bbdd/postgresql/update_from_1.0.4_to_1.0.6_data.sql
@@ -51,3 +51,31 @@ CREATE TABLE efi_organitzacio
  create index efi_organitzacio_pk_i on efi_organitzacio (organitzacioid);
  
  
+
+
+---
+--- 11/05/2023 -   Revisar com s'informen les metadades mínimes obligatòries #266
+---
+
+ INSERT INTO efi_seriedocumental (seriedocumentalid, nom, tipusdocumental, procedimentnom, procedimentcodi) VALUES
+    (1010, 'SD01333', '1', 'Enviafib TD01 Resolució', '---'),
+    (1011, 'SD01334', '2', 'Enviafib TD02 Acords', '---'),
+    (1012, 'SD01335', '3', 'Enviafib TD03 Contractes', '---'),
+    (1013, 'SD01336', '4', 'Enviafib TD04 Convenis', '---'),
+    (1014, 'SD01337', '5', 'Enviafib TD05 Declaracions', '---'),
+    (1015, 'SD01338', '6', 'Enviafib TD06 Comunicacions', '---'),
+    (1016, 'SD01339', '7', 'Enviafib TD07 Notificacions', '---'),
+    (1017, 'SD01340', '8', 'Enviafib TD08 Publicacions', '---'),
+    (1018, 'SD01342', '9', 'Enviafib TD09 Justificants de recepció', '---'),
+    (1019, 'SD01342', '10', 'Enviafib TD10 Actes', '---'),
+    (1020, 'SD01343', '11', 'Enviafib TD11 Certificats', '---'),
+    (1021, 'SD01344', '12', 'Enviafib TD12 Diligències', '---'),
+    (1022, 'SD01345', '13', 'Enviafib TD13 Informes', '---'),
+    (1023, 'SD01346', '14', 'Enviafib TD14 Sol·licituds', '---'),
+    (1024, 'SD01347', '15', 'Enviafib TD15 Denúncies', '---'),
+    (1025, 'SD01348', '16', 'Enviafib TD16 Al·legacions', '---'),
+    (1026, 'SD01349', '17', 'Enviafib TD17 Recursos', '---'),
+    (1027, 'SD01350', '18', 'Enviafib TD18 Comunicacions ciutadà', '---'),
+    (1028, 'SD01351', '19', 'Enviafib TD19 Factures', '---'),
+    (1029, 'SD01352', '20', 'Enviafib TD20 Altre documentació aportada', '---'),
+    (1030, 'SD01353', '99', 'Enviafib TD99 Altres', '---');
