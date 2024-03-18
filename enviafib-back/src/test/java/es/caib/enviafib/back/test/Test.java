ef9d654ebfc9e721f11bc7ce204215a072d7ad84
Antoni Nadal <anadal@fundaciobit.org>
2024-03-18 08:24:05 +0100

    Disponible api de consulta de Revisors d'un Destinatari #382

---
+++ enviafib-back/src/test/java/es/caib/enviafib/back/test/Test.java
@@ -0,0 +1,20 @@
+package es.caib.enviafib.back.test;
+
+import es.caib.enviafib.back.controller.user.LlistatPeticionsUserController;
+/**
+ * REVISORS XYZ 
+ */
+public class Test {
+    public static void main(String[] args) {
+        
+        
+        try {
+            LlistatPeticionsUserController.testRevisors();
+        } catch (Exception e) {
+            // TODO: handle exception
+            e.printStackTrace();
+        }
+        
+    }
+}
+
